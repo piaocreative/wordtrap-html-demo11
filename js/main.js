b6760b3f351367555e254d2720a4daed9f48a006
piaocreative <johnpiao171019@gmail.com>
2023-02-06 12:02:43 -0800

    v2.0.0

--- js/main.js
+++ js/main.js
@@ -1,39 +1,84 @@
 $( document ).ready( function() {
-    
-    if ( $( '.logos' ).length ) {
-        $( '.logos' ).slick({
-            infinite: true,
-            slidesToShow: 5,
-            slidesToScroll: 5,
-            dots: false,
+    // Banner Slider
+	 if ( $('#banner-slider').length ) {
+		$('#banner-slider').slick({
+			dots: false,
             arrows: false,
-            responsive: [
-                {
-                    breakpoint: 992,
-                    settings: {
-                        infinite: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 4,
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        infinite: true,
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        infinite: true,
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                    }
-                }
-            ]
-        });
-    } 
+			infinite: true,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear',
+			autoplay: true
+		});
+	}
+
+    // Product Categories Slider
+	if ( $( '#product_categories' ).length ) {
+		$( '#product_categories' ).slick({
+			infinite: true,
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			dots: false,
+			arrows: false,
+			autoplay: false,
+			responsive: [
+				{
+					breakpoint: 1200,
+					settings: {
+						infinite: true,
+						slidesToShow: 4,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 992,
+					settings: {
+						infinite: true,
+						slidesToShow: 3,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						infinite: true,
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				}
+			]
+		});
+	}
+
+	// Range Slider
+	if ( $( '[data-slider]' ).length ) {
+		$( '[data-slider]' ).slider({});
+
+		$( '#price_slider' ).on( 'slide', function( evt ) {
+			$( '#price_slider_value span' ).html( '$' + evt.value[0] + ' - ' + '$' + evt.value[1] );
+		});
+	}
+
+	// QTY
+	if ( $('.quantity').length ) {
+		$('.quantity').each(function() {
+			var $this = $(this);
+			var $qty = $this.find('.qty');
+			var $plusBtn = $this.find('#plus-btn');
+			var $minusBtn = $this.find('#minus-btn');
+
+			$plusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) + 1 );
+			});
+			$minusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) - 1 );
+				if ($qty.val() == 0) {
+					$qty.val(1);
+				}
+			});
+		});
+	}
 
 } );
